717ddc96e34e4cb0ff4f979ecc54edf56d9d1737
mathangspk <mathangspk@gmail.com>
2025-05-14 21:23:24 +0700

    doi ham tinh adc sang noi suy tuyen tinh 4 doan tu 0V - 3.3 V)

--- include/battery/BatteryMonitor.h
+++ include/battery/BatteryMonitor.h
@@ -6,7 +6,7 @@
 class BatteryMonitor
 {
 public:
-    BatteryMonitor(uint8_t adcPin, float vRef = 3.7466, int adcMax = 4095, float scale = 4.08, float offset = 0);
+    BatteryMonitor(uint8_t adcPin, float vRef = 3.7466, int adcMax = 4095, float scale = 4.072483, float offset = 0);
     void begin();
     float readVoltage(); // Trả về điện áp thực tế (V)
     int readRaw();       // Trả về giá trị ADC thô
@@ -17,6 +17,7 @@ private:
     int _adcMax;
     float _scale;
     float _offset;
+    float interpolate(float x, float x0, float x1, float y0, float y1);
 };
 
 #endif
--- src/battery/BatteryMonitor.cpp
+++ src/battery/BatteryMonitor.cpp
@@ -14,12 +14,30 @@ int BatteryMonitor::readRaw()
     return analogRead(_adcPin);
 }
 
+// Hàm nội suy tuyến tính giữa hai điểm
+float BatteryMonitor::interpolate(float x, float x0, float x1, float y0, float y1) {
+    return y0 + (x - x0) * (y1 - y0) / (x1 - x0);
+}
+
 float BatteryMonitor::readVoltage()
 {
     int raw = readRaw();
-    float adcVoltage = raw * 0.000722 + 0.210;
+    //float adcVoltage = raw * 0.000722 + 0.210;
+    //float voltage = adcVoltage * _scale + _offset;
+    float adcVoltage;
+    //Need to be measure and fill the value
+     if (raw <= 1537) {
+        adcVoltage = interpolate(raw, 1195, 1537, 1.088, 1.40);
+    } else if (raw <= 2804) {
+        adcVoltage = interpolate(raw, 1537, 2804, 1.40, 2.15);
+    } else if (raw <= 3717) {
+        adcVoltage =  interpolate(raw, 2804, 3717, 2.15, 2.85);
+    } else {
+        adcVoltage =  interpolate(raw, 3717, 4304, 2.85, 3.30);
+    }
     float voltage = adcVoltage * _scale + _offset;
-
     //Serial.printf("ADC Raw: %d, Pin Voltage: %.3fV, Battery Voltage: %.3fV\n", raw, adcVoltage, voltage);
     return voltage;
 }
+
+
